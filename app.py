6016238d284dc64996de1e1a67bec28d69ed9662
vijayML2002 <61080679+vijayML2002@users.noreply.github.com>
2021-09-29 16:36:46 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/<timevalue>', methods=['GET', 'POST'])
+def welcome(timevalue):
+    print(timevalue)
+    return "{}".format(timevalue)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
