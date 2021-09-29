6bc25a4db98c55bfcea9bc6ff2c17117ab96eaab
vijayML2002 <61080679+vijayML2002@users.noreply.github.com>
2021-09-29 17:19:41 +0530

    updated app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api/<timevalue>', methods=['GET', 'POST'])
-def welcome(timevalue):
-    print(timevalue)
-    return "{}".format(timevalue)
+#@app.route('/api/<timevalue>', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+    #print(timevalue)
+    #return "{}".format(timevalue)
+    return "hello world"
 
 if __name__ == '__main__':
     app.run()
